e9ded680f21a3c788b6a65f38ec0bcd4e0f29593
Haragucci <tilljan.schrodi787@gmail.com>
2026-06-24 21:33:59 +0200

    Finished verison 1.1.1-beta

--- src/client/java/op/creditmanager/client/command/CreditManagerCommand.java
+++ src/client/java/op/creditmanager/client/command/CreditManagerCommand.java
@@ -1,13 +1,9 @@
 package op.creditmanager.client.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
 import op.creditmanager.client.core.CreditManager;
 import op.creditmanager.client.gui.GuiRouter;
 
@@ -17,23 +13,6 @@ public class CreditManagerCommand {
         registerCommand(dispatcher, manager, "CreditManager");
         registerCommand(dispatcher, manager, "cm");
         registerCommand(dispatcher, manager, "OpCreditManager");
-        dispatcher.register(
-                ClientCommandManager.literal("test-paylog")
-                        .then(ClientCommandManager.argument("betrag", StringArgumentType.word())
-                                .executes(ctx -> {
-                                    String betrag = StringArgumentType.getString(ctx, "betrag");
-
-                                    MinecraftClient client = MinecraftClient.getInstance();
-                                    if (client != null && client.player != null) {
-                                        MutableText finalText = Text.literal("OPSUCHT » leRqven hat dir " + betrag + "$ gegeben.");
-
-                                        client.player.sendMessage(finalText, false);
-                                    }
-
-                                    return 1;
-                                })
-                        )
-        );
     }
 
     private static void registerCommand(CommandDispatcher<FabricClientCommandSource> dispatcher, CreditManager manager, String name) {
